f9fc3459fe7212d560a5e3c0316b5037d35a323e
23033s <23033@students.mrgs.school.nz>
2026-04-30 13:29:55 +1200

    adding and centering start button

--- main.py
+++ main.py
@@ -9,4 +9,9 @@ label = tk.Label(root, image=photo)
 label.image = photo
 label.pack()
 
+start_button = tk.PhotoImage(file="Images/startbutton.png")
+button = tk.Button(root, image=start_button, borderwidth=0, highlightthickness=0, relief="flat")
+
+button.place(relx=0.5, rely=0.6, anchor="center")
+
 root.mainloop()
