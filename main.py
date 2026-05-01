3626f81bcb1d3a4a626581719acb3ca3b3865c02
23033s <23033@students.mrgs.school.nz>
2026-05-01 12:34:55 +1200

    trialing with start button

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 
+def start_action(event=None):
+    print("Started!")
+
 root = tk.Tk()
 
 image = Image.open("Images/homepage.png")
@@ -10,8 +13,9 @@ label.image = photo
 label.pack()
 
 start_button = tk.PhotoImage(file="Images/startbutton.png")
-button = tk.Button(root, image=start_button, borderwidth=0, highlightthickness=0, relief="flat")
-
+button = tk.Button(root, image=start_button, borderwidth=0, highlightthickness=0, relief="flat", cursor="hand2",  command=start_action)
 button.place(relx=0.5, rely=0.6, anchor="center")
+
+root.bind('<Return>', start_action)
 
 root.mainloop()
