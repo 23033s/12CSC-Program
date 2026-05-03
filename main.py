43537b99dbfd9e5fe17e3baeacd3f1739da9af2b
23033s <23033@students.mrgs.school.nz>
2026-05-04 10:50:33 +1200

    hover button and username

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 
-def start_action(event=None):
-    print("Started!")
+
 
 root = tk.Tk()
-
 image = Image.open("Images/homepage.png")
 photo = ImageTk.PhotoImage(image)
 label = tk.Label(root, image=photo)
@@ -13,9 +11,19 @@ label.image = photo
 label.pack()
 
 start_button = tk.PhotoImage(file="Images/startbutton.png")
-button = tk.Button(root, image=start_button, borderwidth=0, highlightthickness=0, relief="flat", cursor="hand2",  command=start_action)
+button = tk.Button(root, image=start_button, relief="flat", cursor="hand2")
 button.place(relx=0.5, rely=0.6, anchor="center")
 
-root.bind('<Return>', start_action)
+def on_enter(event):
+    button.config(bg="#2b58a6")
 
+def on_leave(event):
+    button.config(bg="#182156")
+
+button.bind("<Enter>", on_enter)
+button.bind("<Leave>", on_leave)
+
+username_entry = tk.Entry(root, font=("Arial", 14), bd=2, width=20)
+username_entry.place(relx=0.5, rely=0.5, anchor="center")
+username_entry.insert(0, "Enter Username")
 root.mainloop()
