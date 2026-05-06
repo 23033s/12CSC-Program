684e4a11ea9290bd5e7e46ec9e3dcbb353c18fd8
23033s <23033@students.mrgs.school.nz>
2026-05-06 15:00:04 +1200

    Linking to next window

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 
-
-
 root = tk.Tk()
 image = Image.open("Images/homepage.png")
 photo = ImageTk.PhotoImage(image)
 label = tk.Label(root, image=photo)
 label.image = photo
 label.pack()
+
+def open_next_window():
+    new_window = tk.Toplevel(root)
+    new_window.title("Quiz Main Questions Page")
+    next_img = Image.open("Images/page1.png")
+    next_photo = ImageTk.PhotoImage(next_img)
+    img_label = tk.Label(new_window, image=next_photo)
+    img_label.image = next_photo
+    img_label.pack(pady=10)
+    root.withdraw()
 
 start_button = tk.PhotoImage(file="Images/startbutton.png")
 button = tk.Button(root, image=start_button, relief="flat", cursor="hand2")
@@ -35,11 +43,11 @@ def check_username():
     elif any(char in "!@#$%^&*()-_=+~[]{}|;:'\",<.>/?\\" for char in username):
         outcome_label.config(text="No special characters allowed", fg="red")
     else:
-        outcome_label.config(text=f"Welcome, {username}!", fg="green")
+        outcome_label.config(text=f"Welcome to the quiz, {username}!", fg="green")
+        root.after(2000, open_next_window)
 
 username_entry = tk.Entry(root, font=("Arial", 15), bd=2.5, width=25)
 username_entry.place(relx=0.5, rely=0.54, anchor="center")
 button.config(command=check_username)
-
 
 root.mainloop()
