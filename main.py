c1c32d1c1229a358961c9db5dac103aba3eee89f
23033s <23033@students.mrgs.school.nz>
2026-05-19 09:54:39 +1200

    adding backgrounds and fixing dictionary

--- main.py
+++ main.py
@@ -9,54 +9,28 @@ all_questions= []
 selected_choice= ""
 #dictionary of difficulty of question, questions, answer choices and answers
 quiz_data = {
-    "Easy": [
-        {"question": "What planet is known as the Red Planet?", "choices": ["Earth", "Mars", "Jupiter", "Venus"], "answer": "Mars"},
+        {"question": "What planet is known as the Red Planet?", "choices": ["Earth", "Mars", "Jupiter", "Venus"], "answer": "Mars"}, "background": "Images/q1.png",
         {"question": "What gas do humans need to breathe?", "choices": ["Carbon Dioxide", "Oxygen", "Nitrogen", "Hydrogen"], "answer": "Oxygen"},
         {"question": "Which organ pumps blood in the human body?", "choices": ["Brain", "Lungs", "Heart", "Kidney"], "answer": "Heart"},
         {"question": "Which state of matter has a fixed shape?", "choices": ["Liquid", "Gas", "Solid", "Plasma"], "answer": "Solid"},
-        {"question": "What is the freezing point of water?", "choices": ["0°C", "50°C", "100°C", "-10°C"], "answer": "0°C"}
-    ],
-    "Medium": [
         {"question": "Which blood cell carries oxygen?", "choices": ["White blood cells", "Red blood cells", "Platelets", "Plasma"], "answer": "Red blood cells"},
-        {"question": "Which energy transformation occurs in a moving car?", "choices": ["Chemical → kinetic", "Kinetic → chemical", "Thermal → light", "Light → sound"], "answer": "Chemical → kinetic"},
-        {"question": "Which planet has the strongest gravity?", "choices": ["Saturn", "Earth", "Venus", "Jupiter"], "answer": "Jupiter"},
         {"question": "What is friction mainly caused by?", "choices": ["Gravity", "Surface contact", "Magnetism", "Light"], "answer": "Surface contact"},
-        {"question": "Which part of the cell controls activities?", "choices": ["Cytoplasm", "Nucleus", "Membrane", "Ribosome"], "answer": "Nucleus"}
-    ],
-    "Hard": [
-        {"question": "What is the chemical symbol for potassium?", "choices": ["P", "K", "Pt", "Po"], "answer": "K"},
-        {"question": "What type of bond involves sharing electrons?", "choices": ["Ionic", "Covalent", "Metallic", "Hydrogen"], "answer": "Covalent"},
+        {"question": "Which part of the cell controls activities?", "choices": ["Cytoplasm", "Nucleus", "Membrane", "Ribosome"], "answer": "Nucleus"},
         {"question": "Which part of the brain controls voluntary actions?", "choices": ["Cerebellum", "Cerebrum", "Brainstem", "Neuron"], "answer": "Cerebrum"},
-        {"question": "Which gas is most abundant in Earth’s atmosphere?", "choices": ["Oxygen", "Carbon Dioxide", "Nitrogen", "Hydrogen"], "answer": "Nitrogen"},
-        {"question": "What is the pH of a neutral solution?", "choices": ["0", "7", "14", "10"], "answer": "7"}
-    ],
-    "Challenging": [
         {"question": "In the Heisenberg uncertainty principle, which two properties cannot be observed precisely at the same time?", "choices": ["Velocity and Acceleration", "Mass and Energy", "Position and Momentum", "Spin and Charge"], "answer": "Position and Momentum"},
         {"question": "What is the main purpose of enzymes?", "choices": ["Store energy", "Speed up reactions", "Carry oxygen", "Break bones"], "answer": "Speed up reactions"},
         {"question": "What is the name of the radiation emitted by black holes due to quantum effects?", "choices": ["Synchrotron radiation", "Hawking radiation", "Cherenkov radiation", "Bremsstrahlung"], "answer": "Hawking radiation"},
         {"question": "Which of the following is the SI unit of luminous intensity?", "choices": ["Lumen", "Lux", "Candela", "Watt"], "answer": "Candela"},
-        {"question": "Which type of quark is the heaviest?", "choices": ["Up", "Bottom", "Top", "Charm"], "answer": "Top"}
-    ],
-    "Mix": [
         {"question": "What is the rarest naturally occurring element on Earth?", "choices": ["Uranium", "Astatine", "Francium", "Rhodium"], "answer": "Astatine"},
         {"question": "Which protein is primarily responsible for the maintenance of osmotic pressure in blood?", "choices": ["Gamma globulin", "Immunoglobulin", "Beta macroglobulin", "Albumin"], "answer": "Albumin"},
-        {"question": "What is the chemical symbol for gold?", "choices": ["Au", "Ag", "Gd", "Go"], "answer": "Au"},
         {"question": "What is the powerhouse of the cell?", "choices": ["Nucleus", "Mitochondria", "Ribosome", "Membrane"], "answer": "Mitochondria"},
-        {"question": "What is the chemical formula for water?", "choices": ["CO2", "H2O", "O2", "NaCl"], "answer": "H2O"}
-    ],
-    "TrueFalse": [
-        {"question": "The Moon is bigger than Earth.", "choices": ["True", "False"], "answer": "False"},
-        {"question": "Plants absorb oxygen during photosynthesis.", "choices": ["True", "False"], "answer": "False"},
-        {"question": "A photon has zero mass and cannot exert physical pressure.", "choices": ["True", "False"], "answer": "False"},
-        {"question": "Electricity is a form of energy.", "choices": ["True", "False"], "answer": "True"},
-        {"question": "Momentum depends on both mass and velocity.", "choices": ["True", "False"], "answer": "True"}
-    ]
+
 }
 pyglet.font.add_file("fonts/Fredoka.ttf") #Load the font file from folder
 pyglet.font.add_file("fonts/Agrandir.ttf")#Load the font file from folder
 root = tk.Tk() #create intro window
 root.geometry("1225x690") #resize window
-root.resizable(False, False)
+root.resizable(False, False) #make window unresizable
 root.title("Quiz Homepage") #create a title for window
 image = Image.open("Images/homepage.png") #open the background image file from the image folder
 photo = ImageTk.PhotoImage(image) #convert image to a format so that tkinter can use
